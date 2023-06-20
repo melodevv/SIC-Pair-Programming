b894dc633572a283590499082ef10e0de4100645
Siyabonga Tshabalala <103603030+SiyabongaM225@users.noreply.github.com>
2023-06-20 08:52:19 +0200

    Update main.py

--- Chapter 6/chapterSixUnit32/main.py
+++ Chapter 6/chapterSixUnit32/main.py
@@ -1,13 +1,29 @@
-def maximum_subarray(S):
-    sum = 0
-    for i in range(len(S) - 1):
-        sum += S[i]
-    if sum == 6:
-        return sum
-    else:
-        maximum_subarray(S)
+
+def maximum_subarray(arr):
+    max_sum = float('-inf') # set to negative infinity
+    current_sum = 0
+    start_index = 0
+    end_index = 0
+    current_start_index = 0
+
+    for i in range(len(arr)):
+        current_sum += arr[i]
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            start_index = current_start_index
+            end_index = i
+
+        if current_sum < 0:
+            current_sum = 0
+            current_start_index = i + 1
+
+    return arr[start_index:end_index+1]
 
 
-S = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+S = [-2, 1, -3, 4, -1, 2, -5, 4]
 M = maximum_subarray(S)
 print(M)
+
+
+
