3e354dc8fbcf931a6d0f620a8b48efa89920c659
Siyabonga Tshabalala <103603030+SiyabongaM225@users.noreply.github.com>
2023-06-20 08:06:53 +0200

    Update main.py

--- Chapter 5/chapterFiveUnit29/main.py
+++ Chapter 5/chapterFiveUnit29/main.py
@@ -1,90 +1,43 @@
-from random import randint
-# ========================================
-#
-#               Quick Sort
-#           Divide and Conquer
-#
-# ========================================
-
-def partition1(S, low, high):
-    pivot = S[low]
-    left, right = low + 1, high
-    while left < right:
-        print(S)
-
-        # working on the left side
-        # starting from the leftmost
-        while left <= right and S[left] <= pivot:
-            left += 1
-
-        # working on the right side
-        # starting from the rightmost
-        while left <= right and S[right] >= pivot:
-            right -= 1
-
-        # Perform the swap of elements on the right and left
-        if left < right:
-            S[left], S[right] = S[right], S[left]
-    pivotpoint = right
-    S[low], S[pivotpoint] = S[pivotpoint], S[low]
-    return pivotpoint
+def find_kth_largest_sort(numb, k):
+    numb.sort(reverse=True)
+    return numb[k - 1]
 
 
-def quicksort1(S, low, high):
-    if low < high:
-        print(S)
-        pivotpoint = partition1(S, low, high)
-        quicksort1(S, low, pivotpoint -1)
-        quicksort1(S, pivotpoint +1, high)
+def partition(nums, low, high):
+    pivot = nums[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if nums[j] >= pivot:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+    nums[i + 1], nums[high] = nums[high], nums[i + 1]
+    return i + 1
 
 
-def partition2(S, low, high):
-    rand = randint(low, high)
+def quick_sort(numbers, low, high, k):
+    if low <= high:
+        pivot_point = partition(numbers, low, high)
 
-    # first element changed to a random element
-    S[low], S[rand] = S[rand], S[low]
-    pivot, left, right = S[low], low, high
-    print(S, left, right, "pivot = ", pivot)
-
-    while left < right:
-        while left < high and S[left] <= pivot:
-            left += 1
-        while right > low and pivot <= S[right]:
-            right -= 1
-        if left < right:
-            S[left], S[right] = S[right], S[left]
-    S[low], S[right] = S[right], S[low]
-    return right
+        if pivot_point == k - 1:
+            return numbers[pivot_point]
+        elif pivot_point > k - 1:
+            return quick_sort(numbers, low, pivot_point - 1, k)
+        else:
+            return quick_sort(numbers, pivot_point + 1, high, k)
 
 
-def quicksort2(S, low, high):
-    if low < high:
-        pivotpoint = partition2(S, low, high)
-        quicksort2(S, low, pivotpoint - 1)
-        quicksort2(S, pivotpoint + 1, high)
+def find_kth_largest_partition(num, k):
+    return quick_sort(num, 0, len(num) - 1, k)
 
 
-S = [15, 10, 12, 20, 25, 13, 22]
-# S = [25, 22, 20, 15, 13, 12, 10]
+# Example usage:
+nums = [3, 5, 2, 9, 10]
+k = 3
 
-#
-# First element is used a pivot
-#
-# quicksort1(S, 0, len(S) - 1)
-# print(S)
+kth_largest_sort = find_kth_largest_sort(nums, k)
+kth_largest_partition = find_kth_largest_partition(nums, k)
 
-#
-# Pivot selected at random
-#
-# quicksort2(S, 0, len(S) - 1)
-# print(S)
-
-
-# ========================================
-#
-#               Quick Sort
-#                Pop Quiz
-#
-# ========================================
-partition1(S, 0, len(S) - 1)
-print('answer :', S)
+print("Kth largest element (Sort Method):", kth_largest_sort)
+print("Kth largest element (Partition Method):", kth_largest_partition)
