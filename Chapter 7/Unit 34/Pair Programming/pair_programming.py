bc51f6f494ae41a70bd63a3706e32c5041066df1
Siyabonga Tshabalala <103603030+SiyabongaM225@users.noreply.github.com>
2023-06-24 21:28:01 +0200

    Update pair_programming.py

--- Chapter 7/Unit 34/Pair Programming/pair_programming.py
+++ Chapter 7/Unit 34/Pair Programming/pair_programming.py
@@ -1,5 +1,5 @@
 import turtle
-colors = ['red', 'yellow', 'blue', 'orange', 'green', 'red']
+colors = ['red', 'yellow', 'blue', 'orange', 'green', 'purple']
 aiden = turtle.Turtle()
 turtle.bgcolor('black')
 
@@ -8,4 +8,5 @@ for x in range(360):
     t.pencolor(colors[x % 6])
     t.width(x//100+1)
     t.forward(x)
-    t.left(59)
+    t.left(80)
+    t.speed(5)
